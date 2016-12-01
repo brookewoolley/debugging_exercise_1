41d3fa84c6a46d0d91cd60e5031837d35c39efe6
Mary Rose Cook <mary@maryrosecook.com>
2016-12-01 10:46:12 +0000

    Clarify spec description

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -46,7 +46,7 @@ describe Hand do
     end
   end
 
-  describe "#stand" do
+  describe "#stand and #standing?" do
     it "sets to standing" do
       card_list = double(:card_list, scores: [])
       subject = described_class.new(card_list)
